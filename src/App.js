e0dacef60483788e2c65d96ce3ccaccb94203190
Furqan Butt <furqan.butt@live.co.uk>
2020-11-28 20:33:58 +0100

    Initial commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,24 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import { Route, Switch, Redirect } from "react-router-dom";
+
+import Hamburger from "./components_r/page°/hamburger/Hamburger";
+
+import Home from "./Pages/Home";
+import Movies from "./Pages/Movies";
+import "./styles/App.css"
+
+
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <section className="react-app">
+    <Hamburger/>
+   <Switch> 
+   <Route path ="/movies" component={Movies}/>
+   <Route path ="/" component={Home}/>
+    <Home/>
+    </Switch>
+    </section>
   );
 }
 
---
+++ src/Pages/Movies.jsx
@@ -0,0 +1,9 @@
+import React, { Component } from 'react';
+class Movies extends Component {
+    state = {  }
+    render() { 
+        return ( <section className="content movies"> Movies </section> );
+    }
+}
+ 
+export default Movies;
